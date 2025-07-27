00a56a5f98c126228daf2fea02544d6d7bb0f622
Neel-Pandya <neelpandya2601@gmail.com>
2025-07-27 12:54:10 +0530

    optimized the logic for splash screen navigation

--- Winui Activities/SplashScreen.xaml.cs
+++ Winui Activities/SplashScreen.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,22 +27,15 @@ public sealed partial class SplashScreen : Window
     public SplashScreen()
     {
         InitializeComponent();
-        var timer = new DispatcherTimer();
-        timer.Interval = TimeSpan.FromSeconds(5);
-        timer.Tick += Timer_Tick;
-        timer.Start();
+        ShowMainWindowAfterDelay();
     }
 
-    private void Timer_Tick(object sender, object e)
+    private async void ShowMainWindowAfterDelay()
     {
-        // Stop the timer
-        (sender as DispatcherTimer)?.Stop();
-
-        // Create and show the new window (replace MainWindow with your target window)
-        var mainWindow = new WinUi_Inventory_Management.MainWindow();
-        mainWindow.Activate();
-
-        // Close the splash screen
+        await Task.Delay(3000);
+        new MainWindow().Activate();
         this.Close();
     }
+
+  
 }
