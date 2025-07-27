e8cebba408d77f85c22ceee478bb5b57140597ac
Neel-Pandya <neelpandya2601@gmail.com>
2025-07-27 15:51:52 +0530

    title bar color changed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,30 @@ namespace WinUi_Inventory_Management
         public MainWindow()
         {
             this.InitializeComponent();
+
+            var hwnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hwnd);
+            _appWindow = AppWindow.GetFromWindowId(windowId);
+
+            // Match with dark theme background (recommended values)
+            var titleBar = _appWindow.TitleBar;
+            titleBar.BackgroundColor = Windows.UI.Color.FromArgb(255, 32, 32, 32);  // near WinUI dark background
+            titleBar.ForegroundColor = Colors.White;
+
+            titleBar.ButtonBackgroundColor = Windows.UI.Color.FromArgb(255, 32, 32, 32);
+            titleBar.ButtonForegroundColor = Colors.White;
+
+            titleBar.ButtonHoverBackgroundColor = Windows.UI.Color.FromArgb(255, 64, 64, 64);
+            titleBar.ButtonHoverForegroundColor = Colors.White;
+
+            titleBar.ButtonPressedBackgroundColor = Windows.UI.Color.FromArgb(255, 96, 96, 96);
+            titleBar.ButtonPressedForegroundColor = Colors.White;
+
+            titleBar.InactiveBackgroundColor = Windows.UI.Color.FromArgb(255, 32, 32, 32);
+            titleBar.InactiveForegroundColor = Colors.Gray;
+            titleBar.ButtonInactiveBackgroundColor = Windows.UI.Color.FromArgb(255, 32, 32, 32);
+            titleBar.ButtonInactiveForegroundColor = Colors.Gray;
+
             MainFrame.Navigate(typeof(SplashPage));
         }
     }
