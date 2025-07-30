f2ca83409b0291a6a360c30d441e2ac439866b7d
Hemal Rashiya <142167556+HemalRashiya786@users.noreply.github.com>
2025-07-30 11:15:31 +0530

    Login Ready.

--- Winui Activities/LoginPage.xaml.cs
+++ Winui Activities/LoginPage.xaml.cs
@@ -1,17 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -37,5 +31,65 @@ namespace WinUi_Inventory_Management.Winui_Activities
         {
             Frame.Navigate(typeof(MainLayoutPage));
         }
+
     }
+    
+    //public partial class ValidationFormWidgetViewModel : ObservableValidator
+    //{
+    //    private readonly IDialogService DialogService;
+
+    //    public ValidationFormWidgetViewModel(IDialogService dialogService)
+    //    {
+    //        DialogService = dialogService;
+    //    }
+
+    //    public event EventHandler? FormSubmissionCompleted;
+    //    public event EventHandler? FormSubmissionFailed;
+
+    //    [ObservableProperty]
+    //    [Required]
+    //    [MinLength(2)]
+    //    [MaxLength(100)]
+    //    private string? firstName;
+
+    //    [ObservableProperty]
+    //    [Required]
+
+    //    [MinLength(2)]
+    //    [MaxLength(100)]
+    //    private string? lastName;
+
+    //    [ObservableProperty]
+    //    [Required]
+    //    [EmailAddress]
+    //    private string? email;
+
+    //    [ObservableProperty]
+    //    [Required]
+    //    [Phone]
+    //    private string? phoneNumber;
+
+    //    [RelayCommand]
+    //    private void Submit()
+    //    {
+    //        ValidateAllProperties();
+
+    //        if (HasErrors)
+    //        {
+    //            FormSubmissionFailed?.Invoke(this, EventArgs.Empty);
+    //        }
+    //        else
+    //        {
+    //            FormSubmissionCompleted?.Invoke(this, EventArgs.Empty);
+    //        }
+    //    }
+
+    //    [RelayCommand]
+    //    private void ShowErrors()
+    //    {
+    //        string message = string.Join(Environment.NewLine, GetErrors().Select(e => e.ErrorMessage));
+
+    //        _ = DialogService.ShowMessageDialogAsync("Validation errors", message);
+    //    }
+    //}
 }
